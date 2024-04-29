65fd013de67b311f2365d1267e9b9aa35ba953f7
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-04-29 15:46:25 +0200

    cleanup

--- TestingLowerBounds/KullbackLeibler.lean
+++ TestingLowerBounds/KullbackLeibler.lean
@@ -607,44 +607,6 @@ lemma kl_pi_const {ι : Type*} [hι : Fintype ι] [CountablyGenerated α] [IsPro
   rw [kl_pi, Finset.sum_const, (Finset.card_eq_iff_eq_univ _).mpr, EReal.nsmul_eq_mul]
   rfl
 
---TODO: look for instances of EReal that should be there but are not, and add them, look at the page of ENNReals to see what is there, maybe some stuff is true even for EReals but hasn't been added yet
-
-#check ENNReal.covariantClass_mul_le
---EReal is not covariant wrt the product, e.g. 1 ≤ 2, but -1 * 1 = -1 > -2 = -1 * 2
---however EReal is covariant wrt the product with a nonnegative number, would it make sense to add an instance for this?
---something like EReal.covariantClass_ENNReal_mul_le
-
-#check ENNReal.hasMeasurablePow
---there is no such instance for EReal, but we can derive it from the fact that its a monoid, a mesaurable space and the instance MeasurableMul₂, but we still need to add this last instance
---for MeasurableMul₂ we could pass through ContinuousMul, is it even true for EReal? I think it's true, but I don't know how to add it, maybe it could be a possibility to pass through the fact that it is withTop.
-
-#check ENNReal.instCanonicallyLinearOrderedAddCommMonoidENNReal
-#check ENNReal.instCanonicallyOrderedCommSemiringENNReal
---this is not true for EReal, since the canonical order means that the order is the same as the one defined by subtraction, i.e. `x<y iff y=z+x`, but this is not true if we also have negative numbers
-
-
-instance : CharZero EReal := inferInstanceAs (CharZero (WithBot (WithTop ℝ)))
-#synth CharZero EReal --new
-
--- instance : CanonicallyOrderedCommSemiring EReal := inferInstanceAs (CanonicallyOrderedCommSemiring (WithBot (WithTop ℝ)))
--- #synth CanonicallyOrderedCommSemiring EReal
-
-
-
-
--- #synth Coe EReal Real --already there
-
--- #synth CompleteLinearOrder EReal --already there
-
--- #synth ContinuousAdd EReal
-
--- --I think that the following lemma does not really require the hypothesys of the second space being nonempty, but to modify it I thin I need to change the definition a bit, and then maybe even the following lemmas
--- #check ProbabilityTheory.kernel.borelMarkovFromReal
--- --in any case the pooint is that if the second space is empty I can just define the kernel to be the only measure on the empty space.
--- #synth TopologicalSpace EReal
--- -- #synth NonUnitalSeminormedRing EReal
--- #synth BoundedOrder EReal
--- #synth CanonicallyOrderedAddCommMonoid Real
 
 end Tensorization
 
