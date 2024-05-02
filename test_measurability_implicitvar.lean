358aaba6f498cafa076d5cee09c9d940edac3f19
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-02 14:48:36 +0200

    update test_measurability_implicitvar.lean

--- test_measurability_implicitvar.lean
+++ test_measurability_implicitvar.lean
@@ -1,38 +1,41 @@
 import Mathlib.Probability.Kernel.Basic
 
+/-Issue: if we use the tactic `measurability` in the proof of some result when there is a kernel
+already defined as a variable in our environment, then this kernel becomes a required argument for
+the lemma, even if it is otherwise never mentioned in the statement or proof of the lemma -/
+
 open Real MeasureTheory
 
 variable {α : Type*} {μ : Measure ℝ} {k : ProbabilityTheory.kernel ℝ ℝ}
 
-lemma my_lemma1 (k : ProbabilityTheory.kernel ℝ ℝ) : StronglyMeasurable fun x ↦ x * log x := by
+lemma my_lemma1 : Measurable fun (x : ℝ) ↦ x := by
   measurability
 
 /-
-my_lemma1 {k : ↥(ProbabilityTheory.kernel ℝ ℝ)} (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) :
-  StronglyMeasurable fun x => x * log x
+my_lemma1 {k : ↥(ProbabilityTheory.kernel ℝ ℝ)} (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) : Measurable fun x => x
 -/
 #check my_lemma1
--- In this case the argument k is repeated
+-- In this case k is required as an argument, even if it does not appear in the lemma at all
 
 
-lemma my_lemma2 (k : ProbabilityTheory.kernel ℝ ℝ) : StronglyMeasurable fun x ↦ x * log x := by
-  refine stronglyMeasurable_id.mul measurable_log.stronglyMeasurable
+lemma my_lemma2 : Measurable fun (x : ℝ) ↦ x := by
+  exact fun ⦃t⦄ a => a
 
 /-
-my_lemma2 (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) : StronglyMeasurable fun x => x * log x
+my_lemma2 (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) : Measurable fun x => x
 -/
 #check my_lemma2
--- The problem does not appear if we avoid using the measurability tactic, it is likely caused by
--- the fact that measurability automatically gives a name to the kernel that we defined implicitely,
--- so, even if it is not used in the lemma, it becomes an argument.
+/- The problem does not appear if we avoid using the measurability tactic; it is likely caused by
+the fact that measurability automatically gives a name to the kernel that we defined implicitely,
+so, even if it is not used in the lemma, it becomes an argument. -/
 
 
-lemma my_lemma3 (μ : Measure ℝ) : StronglyMeasurable fun x ↦ x * log x := by
+lemma my_lemma3 {k : ProbabilityTheory.kernel ℝ ℝ} : Measurable fun (x : ℝ) ↦ x := by
   measurability
 
-/-
-my_lemma3 {k : ↥(ProbabilityTheory.kernel ℝ ℝ)} (μ : Measure ℝ) : StronglyMeasurable fun x => x * log x
--/
 #check my_lemma3
--- Here we can notice that the same thing does not happen with the measure, moreover, even if k is
--- not mentioned again in the statement, measurability still picks it up, so it becomes an argument.
+/- If we try to declare the kernel as an argument explicitly in the statement, we end up with a
+lemma that requires both kernels (and they have the same name) -/
+
+/- Note that this does not happen with the measure μ, that is also defined as a variable, just like
+k, so the problem has likely something to do with kernels in  particular. -/
