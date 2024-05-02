d274a6cbb0f7f22d0424a7516a9228676afe138f
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-02 14:55:12 +0200

    Refactor test_measurability_implicitvar.lean

--- test_measurability_implicitvar.lean
+++ test_measurability_implicitvar.lean
@@ -12,7 +12,7 @@ lemma my_lemma1 : Measurable fun (x : ℝ) ↦ x := by
   measurability
 
 /-
-my_lemma1 {k : ↥(ProbabilityTheory.kernel ℝ ℝ)} (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) : Measurable fun x => x
+my_lemma1 {k : ↥(ProbabilityTheory.kernel ℝ ℝ)} : Measurable fun x => x
 -/
 #check my_lemma1
 -- In this case k is required as an argument, even if it does not appear in the lemma at all
@@ -22,7 +22,7 @@ lemma my_lemma2 : Measurable fun (x : ℝ) ↦ x := by
   exact fun ⦃t⦄ a => a
 
 /-
-my_lemma2 (k : ↥(ProbabilityTheory.kernel ℝ ℝ)) : Measurable fun x => x
+my_lemma2 : Measurable fun x => x
 -/
 #check my_lemma2
 /- The problem does not appear if we avoid using the measurability tactic; it is likely caused by
@@ -33,6 +33,9 @@ so, even if it is not used in the lemma, it becomes an argument. -/
 lemma my_lemma3 {k : ProbabilityTheory.kernel ℝ ℝ} : Measurable fun (x : ℝ) ↦ x := by
   measurability
 
+/-
+my_lemma3 {k k : ↥(ProbabilityTheory.kernel ℝ ℝ)} : Measurable fun x => x
+-/
 #check my_lemma3
 /- If we try to declare the kernel as an argument explicitly in the statement, we end up with a
 lemma that requires both kernels (and they have the same name) -/
