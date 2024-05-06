6a973d99251b22acb29e60b4cbd5f0de4ac00db7
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-06 15:24:23 +0200

    cleanup and comment

--- TestingLowerBounds/ForMathlib/MulLog.lean
+++ TestingLowerBounds/ForMathlib/MulLog.lean
@@ -1,26 +1,20 @@
-import Mathlib.Analysis.SpecialFunctions.Log.Deriv
-import Mathlib.Analysis.SpecialFunctions.Pow.Asymptotics
-import Mathlib.Analysis.Convex.Deriv
-
 import Mathlib.MeasureTheory.Function.SpecialFunctions.Basic
 import Mathlib.MeasureTheory.Function.StronglyMeasurable.Basic
-
-open scoped Topology
 
 namespace Real
 
 
---TODO: PR this. First determine if this is actually needed, it would add 2 imports to the file, moreover each lemma can actually be used via the other lemma using just a simple dot notation, so maybe if we add them it would make sense to add just one of them to the file, so that only one additional import is needed, probabily it would make more sense to add the measurable one, since it's slightly shorter and more basic.
---even if we decide not to put this in mathlib it may be good to have the strongly measurable one in the kl file, since it gets used a bungh of times
+--This is probabilty not needed in mathlib, we can leave it in the project for now, but we are not going to PR it
 
-lemma measurable_mul_log : Measurable (fun x ↦ x * log x) := by
-  exact measurable_id.mul measurable_log
+lemma measurable_mul_log : Measurable (fun x ↦ x * log x) :=
+  measurable_id.mul measurable_log
 
-lemma stronglyMeasurable_mul_log : MeasureTheory.StronglyMeasurable (fun x ↦ x * log x) := by
-  exact stronglyMeasurable_id.mul measurable_log.stronglyMeasurable
+lemma stronglyMeasurable_mul_log : MeasureTheory.StronglyMeasurable (fun x ↦ x * log x) :=
+  stronglyMeasurable_id.mul measurable_log.stronglyMeasurable
 
 
 #check measurable_mul_log.stronglyMeasurable
 #check stronglyMeasurable_mul_log.measurable
 
 end Real
+#minimize_imports
