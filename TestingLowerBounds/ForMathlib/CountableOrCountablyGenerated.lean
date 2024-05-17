4c08e9ad68c9238aa1e45092a2b048b8341cfeed
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-17 18:59:48 +0200

    refactor CountableOrCountablyGenerated.lean

--- TestingLowerBounds/ForMathlib/CountableOrCountablyGenerated.lean
+++ TestingLowerBounds/ForMathlib/CountableOrCountablyGenerated.lean
@@ -8,43 +8,62 @@ variable {α β γ : Type*} [MeasurableSpace α] [MeasurableSpace β] [Measurabl
 I cannot put the Nonempty α as a hypothesis because then α would not be mentioned in the thesis
 and that is not allowed in a typeclass instance. Anyway I feel that in this way they are not very
 useful because it seems that the typeclass inference is not able to use them.-/
+--solution: state those as lemmas
 
-instance instOrIsEmptyCountableOfProd [h : Countable (α × β)] : IsEmpty α ∨ Countable β := by
-  by_contra! h1
-  rw [not_isEmpty_iff, not_countable_iff] at h1
-  rcases h1 with ⟨_, _⟩
-  have h' : Uncountable (α × β) := instUncountableProdOfNonempty_1
-  rw [← not_countable_iff] at h'
-  exact h' h
+lemma countable_left_of_prod_of_nonempty [Nonempty β] (h : Countable (α × β)) : Countable α := by
+  contrapose h
+  rw [not_countable_iff] at *
+  infer_instance
 
-instance instOrCountableIsEmptyOfProd [h : Countable (α × β)] : Countable α ∨ IsEmpty β := by
-  by_contra! h1
-  rw [not_isEmpty_iff, not_countable_iff] at h1
-  rcases h1 with ⟨_, _⟩
-  have h' : Uncountable (α × β) := inferInstance
-  rw [← not_countable_iff] at h'
-  exact h' h
+lemma countable_right_of_prod_of_nonempty [Nonempty α] (h : Countable (α × β)) : Countable β := by
+  contrapose h
+  rw [not_countable_iff] at *
+  infer_instance
 
-instance {γ : Type*} [MeasurableSpace γ] [h : CountableOrCountablyGenerated (α × β) γ] :
-    IsEmpty β ∨ CountableOrCountablyGenerated α γ := by
+--TODO: what name should we use? in this name the first 'left' refers to the fact that we are talking about `α`, which is the left factor in the product, the second 'left' refers to the fact that the product is on the left argument of `CountableOrCountablyGenerated`
+lemma countableOrCountablyGenerated_left_of_prod_left_of_nonempty [Nonempty β]
+    [h : CountableOrCountablyGenerated (α × β) γ] :
+    CountableOrCountablyGenerated α γ := by
   rcases h.countableOrCountablyGenerated with (h | h)
-  · by_cases hβ : IsEmpty β
-    · exact Or.inl hβ
-    simp only [hβ, false_or]
-    refine ⟨Or.inl ?_⟩
-    have := instOrCountableIsEmptyOfProd (h := h)
-    tauto
-  · right
+  · have := countable_left_of_prod_of_nonempty h
+    infer_instance
+  · infer_instance
+
+lemma countableOrCountablyGenerated_right_of_prod_left_of_nonempty [Nonempty α]
+    [h : CountableOrCountablyGenerated (α × β) γ] :
+    CountableOrCountablyGenerated β γ := by
+  rcases h.countableOrCountablyGenerated with (h | h)
+  · have := countable_right_of_prod_of_nonempty h
+    infer_instance
+  · infer_instance
+
+--it would be nice to have also the following lemmas. I think they are true, but I cannot be sure,
+--because I cannot prove them even informally.
+--this seems like exactly what I'm looking for:
+--https://math.stackexchange.com/questions/3413063/if-product-sigma-field-is-countably-generated-is-each-factor
+--is it worth it to formalize that proof? I may need to formalize also the hint, if I don't find it on mathlib, this may take a bit of time but I think it may be worth it. However I have to decide whether to do it now or later.
+
+lemma countablyGenerated_left_of_prod_of_nonempty [Nonempty β] (h : CountablyGenerated (α × β)) : CountablyGenerated α := by
+  -- contrapose h
+  sorry
+
+lemma countablyGenerated_right_of_prod_of_nonempty [Nonempty α] (h : CountablyGenerated (α × β)) : CountablyGenerated β := by
+  -- contrapose h
+  sorry
+
+
+lemma countableOrCountablyGenerated_right_of_prod_right_of_nonempty [Nonempty β]
+    [h : CountableOrCountablyGenerated α (β × γ)] :
+    CountableOrCountablyGenerated α γ := by
+  rcases h.countableOrCountablyGenerated with (h | h)
+  · infer_instance
+  · have := countablyGenerated_right_of_prod_of_nonempty h
     infer_instance
 
-instance {γ : Type*} [MeasurableSpace γ] [h : CountableOrCountablyGenerated (α × β) γ] :
-    IsEmpty α ∨ CountableOrCountablyGenerated β γ := by
+lemma countableOrCountablyGenerated_left_of_prod_right_of_nonempty [Nonempty γ]
+    [h : CountableOrCountablyGenerated α (β × γ)] :
+    CountableOrCountablyGenerated α β := by
   rcases h.countableOrCountablyGenerated with (h | h)
-  · by_cases hα : IsEmpty α
-    · exact Or.inl hα
-    simp only [hα, false_or]
-    refine ⟨Or.inl ?_⟩
-    have := instOrIsEmptyCountableOfProd (h := h)
-    tauto
-  · right
+  · infer_instance
+  · have := countablyGenerated_left_of_prod_of_nonempty h
     infer_instance
