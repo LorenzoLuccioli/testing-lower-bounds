ec2815bbdba742c62b8c8b66111ff4946f0d344c
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-20 15:35:19 +0200

    add simp tags

--- TestingLowerBounds/ForMathlib/Integrable_of_empty.lean
+++ TestingLowerBounds/ForMathlib/Integrable_of_empty.lean
@@ -8,9 +8,11 @@ variable {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [h : IsEmp
 --TODO: put this in mathlib, maybe just after:
 #check Integrable.of_finite
 
+@[simp]
 lemma Integrable.of_empty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty α]
     (f : α → β) (μ : Measure α) : Integrable f μ := Integrable.of_finite μ f
 
+@[simp]
 lemma Integrable.of_empty_codomain {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty β]
     {f : α → β} {μ : Measure α} : Integrable f μ := by
   have : IsEmpty α := f.isEmpty
