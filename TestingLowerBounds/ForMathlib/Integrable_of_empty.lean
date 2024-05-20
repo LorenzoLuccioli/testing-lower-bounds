498fac0e2b71b10159fc305b6739377c38e34961
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-20 15:32:14 +0200

    add Integrable.of_empty and Integrable.of_empty_codomain in a separate file

---
+++ TestingLowerBounds/ForMathlib/Integrable_of_empty.lean
@@ -0,0 +1,17 @@
+import Mathlib.MeasureTheory.Function.L1Space
+
+open MeasureTheory
+
+variable {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [h : IsEmpty β]
+    {f : α → β} {μ : Measure α}
+
+--TODO: put this in mathlib, maybe just after:
+#check Integrable.of_finite
+
+lemma Integrable.of_empty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty α]
+    (f : α → β) (μ : Measure α) : Integrable f μ := Integrable.of_finite μ f
+
+lemma Integrable.of_empty_codomain {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty β]
+    {f : α → β} {μ : Measure α} : Integrable f μ := by
+  have : IsEmpty α := f.isEmpty
+  exact Integrable.of_empty f μ
