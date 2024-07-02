5704c445819271b7c765f2e67a9e16c55333f6b7
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-07-02 17:25:06 +0200

    add toReal_statInfo_eq_toReal_sub

--- TestingLowerBounds/Divergences/StatInfo.lean
+++ TestingLowerBounds/Divergences/StatInfo.lean
@@ -29,7 +29,7 @@ open scoped ENNReal NNReal
 namespace ProbabilityTheory
 
 variable {𝒳 𝒳' : Type*} {m𝒳 : MeasurableSpace 𝒳} {m𝒳' : MeasurableSpace 𝒳'}
-  {μ ν : Measure 𝒳} {p : ℝ≥0∞}
+  {μ ν : Measure 𝒳} {p : ℝ≥0∞} (π : Measure Bool)
 
 -- TODO: replace the min by a risk
 noncomputable
@@ -47,14 +47,13 @@ lemma statInfo_comp_le (μ ν : Measure 𝒳) (π : Measure Bool) (η : kernel �
 lemma statInfo_self (μ : Measure 𝒳) (π : Measure Bool) : statInfo μ μ π = 0 := by
   rw [statInfo, bayesBinaryRisk_self, min_mul_mul_right, min_comm, tsub_self]
 
--- lemma toReal_statInfo_eq_toReal_sub (μ ν : Measure 𝒳) (π : Measure Bool) :
---     (statInfo μ ν π).toReal = (min (π {false} * μ Set.univ) (π {true} * μ Set.univ)).toReal
---       - (bayesBinaryRisk μ ν π).toReal := by
---   by_cases h : min (π {false} * μ Set.univ) (π {true} * μ Set.univ) = ⊤
---   ·
---     sorry
---   rw [statInfo, ENNReal.toReal_sub_of_le _ h]
---   exact bayesBinaryRisk_le_min _ _ _
+lemma toReal_statInfo_eq_toReal_sub [IsFiniteMeasure ν] [IsFiniteMeasure π] :
+    (statInfo μ ν π).toReal = (min (π {false} * μ Set.univ) (π {true} * ν Set.univ)).toReal
+      - (bayesBinaryRisk μ ν π).toReal := by
+  rw [statInfo, ENNReal.toReal_sub_of_le]
+  · exact bayesBinaryRisk_le_min _ _ _
+  · simp only [ne_eq, min_eq_top, not_and]
+    exact fun _ ↦  ENNReal.mul_ne_top (measure_ne_top π _) (measure_ne_top ν _)
 
 
 end ProbabilityTheory
