1e265d746acade43053808b81deb8bbbcce19a07
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-07-11 16:40:56 +0200

    fix proof of bayesianRisk_eq_bayesInv_prod

--- TestingLowerBounds/Testing/Risk.lean
+++ TestingLowerBounds/Testing/Risk.lean
@@ -177,16 +177,18 @@ lemma bayesianRisk_eq_bayesInv_prod [StandardBorelSpace Θ] [Nonempty Θ]
     (E : estimationProblem Θ 𝒳 𝒴 𝒵) [IsMarkovKernel E.P] (κ : kernel 𝒳 𝒵)
     (π : Measure Θ) [IsFiniteMeasure π] [IsSFiniteKernel κ] :
     bayesianRisk E κ π = ∫⁻ (θz : Θ × 𝒵), E.ℓ (E.y θz.1, θz.2) ∂(π ∘ₘ (((E.P†π) ×ₖ κ) ∘ₖ E.P)) := by
+  have := E.ℓ_meas
+  have := E.y_meas
   simp only [bayesianRisk, risk]
-  rw [← MeasureTheory.Measure.lintegral_compProd (f := fun θz ↦ E.ℓ (E.y θz.1, θz.2))]
-  swap
-  · have := E.ℓ_meas
-    have := E.y_meas
-    fun_prop
-  -- Measure.comp_prod_left is yet to be proven, it may not be true
-  simp_rw [Measure.compProd_eq_comp, ← Measure.bind_bind', Measure.comp_prod_left, Measure.comp_id]
-  rw [bayesInv_comp_self]
-  simp_rw [Measure.bind_bind']
+  rw [← MeasureTheory.Measure.lintegral_compProd (f := fun θz ↦ E.ℓ (E.y θz.1, θz.2)) (by fun_prop)]
+  congr
+  rw [← kernel.swap_prod, kernel.prod_eq_copy_comp_parallelComp]
+  rw [Measure.compProd_eq_comp, kernel.prod_eq_copy_comp_parallelComp]
+  nth_rw 2 [← kernel.parallelComp_comp_id_right_left]
+  simp_rw [← Measure.comp_assoc]
+  rw [compProd_bayesInv'']
+  simp_rw [Measure.comp_assoc, ← kernel.comp_assoc, kernel.swap_parallelComp, kernel.comp_assoc (_ ∥ₖ κ), kernel.swap_parallelComp, kernel.comp_assoc, kernel.swap_copy, ← kernel.comp_assoc]
+  rw [kernel.parallelComp_comp_id_left_left]
 
 lemma bayesianRisk_ge_lintegral_iInf_bayesInv [StandardBorelSpace Θ] [Nonempty Θ]
     (E : estimationProblem Θ 𝒳 𝒴 𝒵) [IsMarkovKernel E.P] (κ : kernel 𝒳 𝒵)
