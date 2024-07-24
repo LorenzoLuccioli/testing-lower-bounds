d9b758a50f6e9a0e42dc38cbb9a2f7ed23822a27
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-07-24 18:03:57 +0200

    statement and partial proof of generalization of fDiv_eq_integral_fDiv_statInfoFun_of_absolutelyContinuous'

--- TestingLowerBounds/Divergences/StatInfo.lean
+++ TestingLowerBounds/Divergences/StatInfo.lean
@@ -10,6 +10,7 @@ import TestingLowerBounds.FDiv.Basic
 import TestingLowerBounds.Testing.Binary
 import Mathlib.MeasureTheory.Constructions.Prod.Integral
 import TestingLowerBounds.ForMathlib.SetIntegral
+import Mathlib.Analysis.SpecialFunctions.Gamma.BohrMollerup
 
 /-!
 # Statistical information
@@ -754,6 +755,50 @@ lemma fDiv_eq_integral_fDiv_statInfoFun_of_absolutelyContinuous
   rw [← ENNReal.toReal_eq_toReal ENNReal.ofReal_ne_top ENNReal.coe_ne_top, toReal_coe_nnnorm,
     ENNReal.toReal_ofReal (statInfoFun_nonneg _ _ _),
     Real.norm_of_nonneg (statInfoFun_nonneg _ _ _)]
+
+--try to generalize the previous lemma using the lemma itself and something like `fDiv f μ ν = fDiv (x ↦ f x - f 1 * μ univ - rightDeriv f 1 * (μ univ - ν univ)) μ ν + ...`
+
+#check fDiv_add_const
+#check fDiv_add_linear'
+
+--swap the names when I'm done
+lemma fDiv_eq_integral_fDiv_statInfoFun_of_absolutelyContinuous'
+    [IsFiniteMeasure μ] [IsFiniteMeasure ν]
+    (hf_cvx : ConvexOn ℝ univ f) (hf_cont : Continuous f)
+    (h_int : Integrable (fun x ↦ f ((∂μ/∂ν) x).toReal) ν)
+    (h_ac : μ ≪ ν) :
+    fDiv f μ ν = ∫ x, (fDiv (statInfoFun 1 x) μ ν).toReal ∂(curvatureMeasure hf_cvx)
+      + f 1 * ν univ + rightDeriv f 1 * (μ univ - ν univ) := by
+  have : fDiv f μ ν = fDiv (fun x ↦ f x - f 1 - rightDeriv f 1 * (x - 1)) μ ν
+      + f 1 * ν univ + rightDeriv f 1 * ((μ univ).toReal - (ν univ).toReal) := by
+    simp_rw [sub_eq_add_neg (f _), sub_eq_add_neg (_ + _), ← neg_mul]
+    rw [fDiv_add_linear']
+    swap; · exact hf_cvx.subset (fun _ _ ↦ trivial) (convex_Ici 0) |>.add_const _
+    rw [fDiv_add_const]
+    swap; · exact hf_cvx.subset (fun _ _ ↦ trivial) (convex_Ici 0)
+    simp_rw [EReal.coe_neg, neg_mul]
+    rw [add_assoc, add_comm (_ * _), ← add_assoc, add_assoc _ (-(_ * _)), add_comm (-(_ * _)),
+      ← sub_eq_add_neg (_ * _), EReal.sub_self, add_zero]
+    rotate_left
+    · refine (EReal.mul_ne_top _ _).mpr ⟨?_, Or.inr <| EReal.add_top_iff_ne_bot.mp rfl,
+        ?_, Or.inr <| Ne.symm (ne_of_beq_false rfl)⟩ <;> simp
+    · refine (EReal.mul_ne_bot _ _).mpr ⟨?_, Or.inr <| EReal.add_top_iff_ne_bot.mp rfl,
+        ?_, Or.inr <| Ne.symm (ne_of_beq_false rfl)⟩ <;> simp
+    rw [add_assoc, add_comm (-(_ * _)), ← sub_eq_add_neg, EReal.sub_self, add_zero]
+    · refine (EReal.mul_ne_top _ _).mpr ?_
+      simp [measure_ne_top]
+    · refine (EReal.mul_ne_bot _ _).mpr ?_
+      simp [measure_ne_top]
+  rw [this]
+  congr
+  ·
+    -- rw [fDiv_eq_integral_fDiv_statInfoFun_of_absolutelyContinuous]
+
+    sorry
+  ·
+    sorry
+  ·
+    sorry
 
 
 --this will be useful later
